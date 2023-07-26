68b369f51f59feca41cf1c5cb79401b6ec93ed5e
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-26 03:38:01 -0700

    modify the js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,48 @@
-import './style.css';
+import "./style.css";
 
+const dataArray = [];
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputValue1 = document.getElementById("name").value;
+  const inputValue2 = document.getElementById("score").value;
+
+  const userInputObject = {
+    input1: inputValue1,
+    input2: inputValue2,
+  };
+
+  dataArray.push(userInputObject);
+
+  document.getElementById('score').value = '';
+  document.getElementById('name').value = '';
+   
+  displayData();
+});
+
+function displayData() {
+    const dataList = document.getElementById('displayData');
+    dataList.innerHTML = '';
+
+    dataArray.forEach(data => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `Input 1: ${data.input1}, Input 2: ${data.input2}`
+        dataList.appendChild(listItem);
+    })
+
+}
+
+
+// function storeData() {
+//     const inputValue1 = document.getElementById('name');
+//     const inputValue2 = document.getElementById('score');
+
+//     const userInputObject = {
+//         input1: inputValue1,
+//         input2: inputValue2,
+//     };
+
+//     dataArray.push(userInputObject);
+
+// }}
