d66ada21818d6abfd0b011a3f8fdb4660376ef3a
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-28 02:28:33 -0700

    create api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import './style.css';
-import { storeData, initDataFromLocalStorage } from './modules/formDataModule.js';
-import displayData from './modules/displayModule.js';
+import { addScore, fetchData } from './modules/addScore.js';
 
-initDataFromLocalStorage();
-displayData();
-const form = document.getElementById('form');
-form.addEventListener('submit', storeData);
+const scoreList = document.querySelector('.lists');
+const form = document.querySelector('#add-new');
+const refresh = document.querySelector('.refresh');
+
+const displayLists = async () => {
+  const scores = await fetchData();
+  scoreList.innerHTML = '';
+  scores.forEach((data) => {
+    scoreList.innerHTML += `
+        <li><span class="list-name">${data.user}</span><span class="listscore">${data.score}</span></li>`;
+  });
+};
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const name = document.querySelector('#leader-name').value.trim();
+  const score = document.querySelector('#leader-score').value.trim();
+
+  if (name !== '' && score !== '') {
+    const scoreData = {
+      user: name,
+      score,
+    };
+    await addScore(scoreData);
+    document.querySelector('#leader-name').value = '';
+    document.querySelector('#leader-score').value = '';
+  }
+});
+
+refresh.addEventListener('click', displayLists);
---
+++ src/modules/addScore.js
@@ -0,0 +1,20 @@
+const uniqueId = "Zelalem1234"; // Replace this with a unique ID of your choice
+const apiUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${uniqueId}/scores/`;
+
+const addScore = async (scoreList) => {
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(scoreList),
+  });
+  const data = await response.json();
+  return data.result;
+};
+
+const fetchData = async () => {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data.result;
+};
+
+export { addScore, fetchData };
