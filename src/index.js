8a54308ca5d4e805a035d304b795def21e7cd4bb
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-28 02:34:45 -0700

    modify the js and css file

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import './style.css';
 import { addScore, fetchData } from './modules/addScore.js';
 
 const scoreList = document.querySelector('.lists');
+
 const form = document.querySelector('#add-new');
 const refresh = document.querySelector('.refresh');
 
 const displayLists = async () => {
   const scores = await fetchData();
   scoreList.innerHTML = '';
-  scores.forEach((data) => {
-    scoreList.innerHTML += `
-        <li><span class="list-name">${data.user}</span><span class="listscore">${data.score}</span></li>`;
+  scores.forEach((data, index) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+        <span class="list-name">${data.user}</span>
+        <span class="list-score">${data.score}</span>`;
+
+    if (index % 2 !== 0) {
+      listItem.classList.add('even');
+    }
+    scoreList.appendChild(listItem);
   });
 };
+
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
